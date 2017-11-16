c0af6ef20289514b973a68aa9ef4a979ef9bfc7b
苏剑林 <bojone@spaces.ac.cn>
2017-11-16 02:34:10 -0600

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -4,4 +4,4 @@ mkdir output
 build/vocab_count -min-count 16 < tmp/corpus.txt > tmp/corpus_vocab.glove
 build/cooccur -memory 30 -vocab-file tmp/corpus_vocab.glove -window-size 10 < tmp/corpus.txt > tmp/corpus_cooccur.glove
 build/shuffle -memory 30 < tmp/corpus_cooccur.glove > tmp/corpus_cooccur_shuffle.glove
-build/glove -threads 8 -vector-size 128 -binary 2 -model 0 -write-header 1 -iter 25 -input-file tmp/corpus_cooccur_shuffle.glove -vocab-file tmp/corpus_vocab.glove -save-file output/baike.128.glove
+build/glove -threads 16 -vector-size 128 -binary 2 -write-header 1 -iter 25 -input-file tmp/corpus_cooccur_shuffle.glove -vocab-file tmp/corpus_vocab.glove -save-file output/baike.128.glove
