94ffa029cff3e2922802265d7c91734b160873e8
苏剑林 <bojone@spaces.ac.cn>
2017-11-16 18:49:59 -0600

    Create doc2txt.py

---
+++ doc2txt.py
@@ -0,0 +1,21 @@
+#! -*- coding:utf-8 -*-
+# An example script to export corpus as the format glove need.
+
+from tqdm import tqdm
+
+import pymongo
+db = pymongo.MongoClient().baike_lab.words2
+
+class Document:
+    def __iter__(self):
+        for t in db.find(no_cursor_timeout=True).limit(500000):
+            yield t['summary']
+            yield t['content']
+
+import codecs
+f = codecs.open('tmp/corpus.txt', 'wa', encoding='utf-8')
+for d in tqdm(Document()):
+    f.write(' '.join(d))
+    f.write('\n')
+
+f.close()
