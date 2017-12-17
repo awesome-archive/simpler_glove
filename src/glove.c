b63119d416e2b68e38ce738072db15ef3f1181f9
苏剑林 <bojone@spaces.ac.cn>
2017-12-17 04:02:26 -0600

    Update glove.c

--- src/glove.c
+++ src/glove.c
@@ -67,21 +67,36 @@ int scmp( char *s1, char *s2 ) {
 
 void initialize_parameters() {
     long long a, b;
-
-    /* Allocate space for word vectors and context word vectors, and correspodning gradsq */
-    a = posix_memalign((void **)&W, 128, vocab_size * vector_size * sizeof(real)); // Might perform better than malloc
-    if (W == NULL) {
-        fprintf(stderr, "Error allocating memory for W\n");
-        exit(1);
+    
+    if (model ==0){
+        /* Allocate space for word vectors and context word vectors, and correspodning gradsq */
+        a = posix_memalign((void **)&W, 128, vocab_size * vector_size * sizeof(real)); // Might perform better than malloc
+        if (W == NULL) {
+            fprintf(stderr, "Error allocating memory for W\n");
+            exit(1);
+        }
+        a = posix_memalign((void **)&gradsq, 128, vocab_size * vector_size * sizeof(real)); // Might perform better than malloc
+        if (gradsq == NULL) {
+            fprintf(stderr, "Error allocating memory for gradsq\n");
+            exit(1);
+        }
+        for (b = 0; b < vector_size; b++) for (a = 0; a < vocab_size; a++) W[a * vector_size + b] = (rand() / (real)RAND_MAX - 0.5) / vector_size;
+        for (b = 0; b < vector_size; b++) for (a = 0; a < vocab_size; a++) gradsq[a * vector_size + b] = 1.0; // So initial value of eta is equal to initial learning rate
+    } else {
+        /* Allocate space for word vectors and context word vectors, and correspodning gradsq */
+        a = posix_memalign((void **)&W, 128, 2 * vocab_size * vector_size * sizeof(real)); // Might perform better than malloc
+        if (W == NULL) {
+            fprintf(stderr, "Error allocating memory for W\n");
+            exit(1);
+        }
+        a = posix_memalign((void **)&gradsq, 128, 2 * vocab_size * vector_size * sizeof(real)); // Might perform better than malloc
+        if (gradsq == NULL) {
+            fprintf(stderr, "Error allocating memory for gradsq\n");
+            exit(1);
+        }
+        for (b = 0; b < vector_size; b++) for (a = 0; a < 2 * vocab_size; a++) W[a * vector_size + b] = (rand() / (real)RAND_MAX - 0.5) / vector_size;
+        for (b = 0; b < vector_size; b++) for (a = 0; a < 2 * vocab_size; a++) gradsq[a * vector_size + b] = 1.0; // So initial value of eta is equal to initial learning rate
     }
-    a = posix_memalign((void **)&gradsq, 128, vocab_size * vector_size * sizeof(real)); // Might perform better than malloc
-    if (gradsq == NULL) {
-        fprintf(stderr, "Error allocating memory for gradsq\n");
-        exit(1);
-    }
-    for (b = 0; b < vector_size; b++) for (a = 0; a < vocab_size; a++) W[a * vector_size + b] = (rand() / (real)RAND_MAX - 0.5) / vector_size;
-    for (b = 0; b < vector_size; b++) for (a = 0; a < vocab_size; a++) gradsq[a * vector_size + b] = 1.0; // So initial value of eta is equal to initial learning rate
-
 }
 
 void read_vocab_file() {
